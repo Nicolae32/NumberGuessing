0027c26daa96e5f623ba4f932388aa31740277c6
Stoicoi Nicolae <stoicoinicolae4@gmail.com>
2024-05-13 09:21:02 +0300

    number guessing 50% complete

---
+++ number_guessing/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace number_guessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 12);
+            this.txt.Multiline = true;
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(190, 273);
+            this.txt.TabIndex = 2;
+            this.txt.TextChanged += new System.EventHandler(this.Txt_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(298, 75);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.TextBox txt2;
+    }
+}
+
---
+++ number_guessing/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace number_guessing
+{
+    public partial class Form1 : Form
+    {
+        Random number = new Random();
+        public Form1()
+        {
+
+            
+            InitializeComponent();
+        }
+        
+        private void FlowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Verify(txt2.Text);
+            
+        }
+
+        private void Txt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void Verify(string f)
+        {
+            
+            int n = number.Next(1,10);
+            int num = int.Parse(f);
+            if(n == num)
+            {
+                txt.Text = "Felicitari ai ghicit numarul!";
+
+            }
+            if(n>num)
+            {
+                txt.Text = "Mai mare!";
+               
+
+            }
+            if(n>num)
+            {
+                txt.Text = "Mai mic!";
+
+            }
+
+        }
+    }
+}
